da487ead4234008539ee40bf03b5cb56c5322cce
ritavash01 <94717544+ritavash01@users.noreply.github.com>
2024-11-26 17:44:17 +0530

    Update interfaces.py

--- src/candies/interfaces.py
+++ src/candies/interfaces.py
@@ -1,97 +1,54 @@
-"""
-I/O interfaces for candies.
-"""
+try: 
+    from typing import Self
+except ImportError: 
+    from typing_extensions import Self 
 
-import mmap
-from pathlib import Path
 from dataclasses import dataclass
 
-import numpy as np
-from priwo import readhdr, writefil
+import numpy as np 
+#import the two nanobind functions, something like import shm_reader
+
 
 from candies.utilities import dm2delay
 from candies.base import CandiesError, Candidate
 
 
-@dataclass
-class SIGPROCFilterbank:
-    """
-    Context manager to read data from a SIGPROC filterbank file.
-    """
+@dataclass    
+class Getrawdata: 
+    
 
-    filepath: str | Path
+    def getdatabuffer(count, offset): 
+        data = shm_reader.get_data_as_numpy_array(count,offset)
 
-    def __enter__(self):
-        self.header = readhdr(str(self.filepath))
-
-        try:
-            self.fh = self.header["fch1"]
-            self.df = self.header["foff"]
-            self.dt = self.header["tsamp"]
-            self.nf = self.header["nchans"]
-            self.nskip = self.header["size"]
-            self.nbits = self.header["nbits"]
-        except KeyError:
-            CandiesError("File may not be a valid SIGPROC filterbank file.")
-
-        self.fobject = open(self.filepath, mode="rb")
-
-        if self.df < 0:
-            self.df = abs(self.df)
-            self.bw = self.nf * self.df
-            self.fl = self.fh - self.bw + (0.5 * self.df)
-        else:
-            self.fl = self.fh
-            self.bw = self.nf * self.df
-            self.fh = self.fl + self.bw - (0.5 * self.df)
-
-        self.dtype = {
-            8: np.uint8,
-            16: np.uint16,
-            32: np.float32,
-            64: np.float64,
-        }[self.nbits]
-
-        self.mapped = mmap.mmap(
-            self.fobject.fileno(),
-            0,
-            mmap.MAP_PRIVATE,
-            mmap.PROT_READ,
-        )
-
-        self.nt = int(int(self.mapped.size() - self.nskip) / self.nf)
-
-        return self
-
-    def __exit__(self, *args) -> None:
-        if self.fobject:
-            self.fobject.close()
-
-    def get(self, offset: int, count: int) -> np.ndarray:
-        """
-        Get data from this file.
-        """
-
-        data = np.frombuffer(
-            self.mapped,
-            dtype=self.dtype,
-            count=count * self.nf,
-            offset=offset * self.nf + self.nskip,
-        )
-        data = data.reshape(-1, self.nf)
-        data = data.T
         return data
 
-    def chop(self, candidate: Candidate) -> tuple[float, float, np.ndarray]:
-        """
-        Chops out a candy-date from this file.
+    def getdataheader():
+        hdr_dict = hdr_shm_reader.initialize_HDR_SHM_py()
 
-        Parameters
-        ----------
-        candidate: Candidate
-            The candy-date to chop.
-        """
+        return hdr_dict
+    try: 
 
+        self.fh = hdr_dict["Frequency_Ch_0_Hz"]
+        self.df = hdr_dict["Channel_width_Hz"] / 1e6
+        self.dt = hdr_dict["Sampling_time_uSec"]
+        self.nf = hdr_dict["Channels"]
+        self.bw = hdr_dict["Bandwidth_MHz"]
+        self.nbits = hdr_dict["Num_bits_per_sample"]
+        self.nt = 4*100*1e6/ self.nf  #Instead of check the memory of filterbank we take 4 blocks of FRB SHM
+        
+    except:
+          CandiesError("Dictionary reading was incorrect")
+
+        
+    if self.df < 0: 
+        self.df = abs(self.df)
+        self.fl = self.fh - self.bw + (0.5 * self.df)
+    else: 
+        self.fl = self.fh 
+        self.fh = self.fl + self.bw - (0.5 * self.df)
+
+
+    def chop(self, candidate: Candidate) -> np.ndarray:
         maxdelay = dm2delay(self.fl, self.fh, candidate.dm)
         binbeg = int((candidate.t0 - maxdelay) / self.dt) - candidate.wbin
         binend = int((candidate.t0 + maxdelay) / self.dt) + candidate.wbin
@@ -105,38 +62,21 @@ class SIGPROCFilterbank:
         nbegin = noffset - (nread - ncount) // 2
 
         if (nbegin >= 0) and (nbegin + nread) <= self.nt:
-            data = self.get(offset=nbegin, count=nread)
+            data = self.getdatabuffer(offset=nbegin, count=nread)
         elif nbegin < 0:
             if (nbegin + nread) <= self.nt:
-                d = self.get(offset=0, count=nread + nbegin)
+                d = self.getdatabuffer(offset=0, count=nread + nbegin)
                 dmedian = np.median(d, axis=1)
                 data = np.ones((self.nf, nread), dtype=self.dtype) * dmedian[:, None]
                 data[:, -nbegin:] = d
             else:
-                d = self.get(offset=0, count=self.nt)
+                d = self.getdatabuffer(offset=0, count=self.nt)
                 dmedian = np.median(d, axis=1)
                 data = np.ones((self.nf, nread), dtype=self.dtype) * dmedian[:, None]
                 data[:, -nbegin : -nbegin + self.nt] = d
         else:
-            d = self.get(offset=nbegin, count=self.nt - nbegin)
+            d = self.getdatabuffer(offset=nbegin, count=self.nt - nbegin)
             dmedian = np.median(d, axis=1)
             data = np.ones((self.nf, nread), dtype=self.dtype) * dmedian[:, None]
             data[:, : self.nt - nbegin] = d
-        tbeg = nbegin * self.dt
-        tend = tbeg + (nread * self.dt)
-        return tbeg, tend, data
-
-    def store(self, candidate: Candidate) -> None:
-        """
-        Store a candy-date from this file.
-
-        Parameters
-        ----------
-        candidates: Candidate
-            The candy-date to store.
-        """
-        meta = self.header.copy()
-        meta["refdm"] = candidate.dm
-        tbeg, _, data = self.chop(candidate)
-        meta["tstart"] = meta["tstart"] + tbeg / 86400.0
-        writefil(meta, data, "".join([str(candidate), ".fil"]))
+        return data
