4e8591b6c09d948005c65860e50eb519da558b10
ritavash01 <94717544+ritavash01@users.noreply.github.com>
2024-11-27 10:46:27 +0530

    Update interfaces.py

--- src/candies/interfaces.py
+++ src/candies/interfaces.py
@@ -1,52 +1,51 @@
-try: 
-    from typing import Self
-except ImportError: 
-    from typing_extensions import Self 
-
+from typing import Self
 from dataclasses import dataclass
-
-import numpy as np 
+import numpy as np
 import internals
-
-
 from candies.utilities import dm2delay
 from candies.base import CandiesError, Candidate
 
 
-@dataclass    
-class Getrawdata: 
+@dataclass
+class Getrawdata:
     
+    fh: float = None
+    df: float = None
+    dt: float = None
+    nf: int = None
+    bw: float = None
+    nbits: int = None
+    nt: float = None
+    dtype: np.dtype = np.float32  # Or set an appropriate data type
 
-    def getdatabuffer(count, offset): 
-        data = internals.get_data_as_numpy_array(count,offset)
+    def __post_init__(self):
+        hdr_dict = self.getdataheader()
 
+        try:
+            self.fh = hdr_dict["Frequency_Ch_0_Hz"]
+            self.df = hdr_dict["Channel_width_Hz"] / 1e6
+            self.dt = hdr_dict["Sampling_time_uSec"]
+            self.nf = hdr_dict["Channels"]
+            self.bw = hdr_dict["Bandwidth_MHz"]
+            self.nbits = hdr_dict["Num_bits_per_sample"]
+            self.nt = 4 * 100 * 1e6 / self.nf  # Adjust memory block size if needed
+        except KeyError:
+            raise CandiesError("Dictionary reading was incorrect")
+
+        if self.df < 0:
+            self.df = abs(self.df)
+            self.fl = self.fh - self.bw + (0.5 * self.df)
+        else:
+            self.fl = self.fh
+            self.fh = self.fl + self.bw - (0.5 * self.df)
+
+    def getdatabuffer(self, count, offset):
+        data = internals.get_data_as_numpy_array(count, offset)
         return data
 
-    def getdataheader():
+    def getdataheader(self):
         hdr_dict = internals.initialize_HDR_SHM_py()
-
         return hdr_dict
-    try: 
-
-        self.fh = hdr_dict["Frequency_Ch_0_Hz"]
-        self.df = hdr_dict["Channel_width_Hz"] / 1e6
-        self.dt = hdr_dict["Sampling_time_uSec"]
-        self.nf = hdr_dict["Channels"]
-        self.bw = hdr_dict["Bandwidth_MHz"]
-        self.nbits = hdr_dict["Num_bits_per_sample"]
-        self.nt = 4*100*1e6/ self.nf  #Instead of check the memory of filterbank we take 4 blocks of FRB SHM
-        
-    except:
-          CandiesError("Dictionary reading was incorrect")
-
-        
-    if self.df < 0: 
-        self.df = abs(self.df)
-        self.fl = self.fh - self.bw + (0.5 * self.df)
-    else: 
-        self.fl = self.fh 
-        self.fh = self.fl + self.bw - (0.5 * self.df)
-
 
     def chop(self, candidate: Candidate) -> np.ndarray:
         maxdelay = dm2delay(self.fl, self.fh, candidate.dm)
@@ -73,10 +72,10 @@ class Getrawdata:
                 d = self.getdatabuffer(offset=0, count=self.nt)
                 dmedian = np.median(d, axis=1)
                 data = np.ones((self.nf, nread), dtype=self.dtype) * dmedian[:, None]
-                data[:, -nbegin : -nbegin + self.nt] = d
+                data[:, -nbegin: -nbegin + self.nt] = d
         else:
             d = self.getdatabuffer(offset=nbegin, count=self.nt - nbegin)
             dmedian = np.median(d, axis=1)
             data = np.ones((self.nf, nread), dtype=self.dtype) * dmedian[:, None]
-            data[:, : self.nt - nbegin] = d
+            data[:, :self.nt - nbegin] = d
         return data
